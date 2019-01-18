9acde065406d97cca31d8e4aca917e82f97be057
Tom Settle <tsettle44@gmail.com>
2019-01-17 21:01:15 -0500

    prepare for dev

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const Cred = require("./routes/api/cred");
+const Cred = require("./server/routes/api/cred");
+const path = require("path");
 var mongoose = require("mongoose");
 
 // mongodb connection
@@ -19,8 +20,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //Posts API
-const posts = require("./routes/api/post");
+const posts = require("./server/routes/api/post");
 app.use("/api/posts", posts);
+
+//Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
